f74cc3072c6e9acf1709f2f34fbbbff35f69b166
Ederjoel26 <eder.joel55@gmail.com>
2026-02-10 22:58:27 -0600

    feat: another solution with time O(n log n) and space O(1)

--- roadmap/array-and-hashing/contains-duplicate.cpp
+++ roadmap/array-and-hashing/contains-duplicate.cpp
@@ -1,7 +1,25 @@
+// Time: O(n)
+// Space: O(n)
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
       unordered_set<int> s(nums.begin(), nums.end());
-      return nums.size() > s.size();
+      return nums.size() > s.size()
     }
 };
+
+
+// Time: O(n log n)
+// Space: O(1)
+// class Solution {
+// public:
+//     bool hasDuplicate(vector<int>& nums) {
+//       sort(nums.begin(), nums.end());
+//       for (int i = 1; i < nums.size(); i++) {
+//         if (nums[i - 1] == nums[i]) {
+//           return true;
+//         }
+//       }
+//       return false;
+//     }
+// };
