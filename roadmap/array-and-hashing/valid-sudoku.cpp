06f28239029ab3f0ec976a2b95fdd831fcad8b83
Ederjoel26 <eder.joel55@gmail.com>
2026-02-19 00:23:45 -0600

    feat: add valid-sudoku solution and complete products-of-array-except-self

---
+++ roadmap/array-and-hashing/valid-sudoku.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+      unordered_map h;
+      for (vector<char>& col: board) {
+        for(char& rowE: col) {
+        }
+      }
+        
+    }
+};
